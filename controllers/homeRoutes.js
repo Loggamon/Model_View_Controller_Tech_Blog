4a1f7f6bc71a714769ae178c97598b03192ad997
Logan Monson <scarletfedora@gmail.com>
2022-05-26 01:13:18 -0700

    got handlebars to work!

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,13 +1,33 @@
-const router = require('express').Router();
-const { User } = require('../models');
+const router = require("express").Router();
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
-   //temp test
-   try {
-   res.render('homepage')
-   } catch (err) {
+router.get("/", async (req, res) => {
+  //temp test
+  try {
+    res.render("homepage");
+  } catch (err) {
     res.status(500).json(err);
   }
+});
+
+router.get("/dashboard", withAuth, async (req, res) => {
+  //temp test
+  try {
+    res.render("dashboard");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", async (req, res) => {
+  //temp test
+  if (req.session.logged_in) {
+    res.redirect("/dashboard");
+    return;
+  }
+
+  res.render("login");
 });
 
 module.exports = router;
