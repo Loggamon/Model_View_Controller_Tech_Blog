719882d29bcc3e0ad597f3de3893b8ea98a2a250
unknown <scarletfedora@gmail.com>
2022-05-29 22:13:04 -0700

    All routes work

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,25 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-  //temp test
   try {
-    res.render('homepage');
+
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in
+    });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -16,7 +30,7 @@ router.get('/dashboard', withAuth, async (req, res) => {
   try {
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      // include: [{ model: Post }],
+      include: [{ model: Post }],
     });
 
     const user = userData.get({ plain: true });
