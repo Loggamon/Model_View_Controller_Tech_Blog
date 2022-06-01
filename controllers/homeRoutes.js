3d84a54f32ba54775e2e281153137ccb8a9ee72a
Logan Monson <scarletfedora@gmail.com>
2022-06-01 03:02:39 -0700

    got the comments to save w/ user_id and post_id... now i need to render them

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,42 +1,51 @@
-const router = require('express').Router();
-const { User, Post, Comment } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User, Post, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
-
     const postData = await Post.findAll({
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["name"],
         },
-  
       ],
     });
 
     const posts = postData.map((post) => post.get({ plain: true }));
 
-    res.render('homepage', {
+    res.render("homepage", {
       posts,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/post/:id', async (req, res) => {
+router.get("/post/:id", async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["name", "email"],
         },
         //
         {
           model: Comment,
+          attributes: [
+            "description",
+            "user_id",
+            "post_id",
+            "date_created",
+            "id",
+          ],
+          include: {
+            model: User,
+            attributes: ["name"],
+          },
         },
         //
       ],
@@ -44,28 +53,28 @@ router.get('/post/:id', async (req, res) => {
 
     const post = postData.get({ plain: true });
 
-    res.render('post', {
+    res.render("post", {
       ...post,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/dashboard', withAuth, async (req, res) => {
+router.get("/dashboard", withAuth, async (req, res) => {
   //temp test
   try {
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
+      attributes: { exclude: ["password"] },
       include: [{ model: Post }],
     });
 
     const user = userData.get({ plain: true });
 
-    res.render('dashboard', {
+    res.render("dashboard", {
       ...user,
-      logged_in: true
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -73,13 +82,12 @@ router.get('/dashboard', withAuth, async (req, res) => {
 });
 
 router.get("/login", (req, res) => {
-
   if (req.session.logged_in) {
-    res.redirect('/dashboard');
+    res.redirect("/dashboard");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
 module.exports = router;
--- public/js/comment.js
+++ public/js/comment.js
@@ -1,31 +1,30 @@
-const newCommentHandler =  async (event) => {
-    event.preventDefault();
+const newCommentHandler = async (event) => {
+  event.preventDefault();
 
-    const description = document.querySelector('#comment-desc').value.trim();
-    const post_id = event.target.getAttribute('data-id');
+  const description = document.querySelector("#comment-desc").value.trim();
+  const post_id = document.querySelector('input[name="post-id"]').value;
 
-    console.log(post_id);
+  console.log(description);
+  console.log(post_id);
 
-
-    if (description) {
-
-        const response = await fetch(`/api/comments`, {
-            method: 'POST',
-            body: JSON.stringify({ description }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-        console.log(response);
-        if (response.ok) {
-            //document.location.replace('/');
-            console.log(response);
-        } else {
-            alert('Oops! There was an issue commenting on this post!');
-        }
+  if (description) {
+    const response = await fetch(`/api/comments`, {
+      method: "POST",
+      body: JSON.stringify({ description, post_id }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(response);
+    if (response.ok) {
+      document.location.replace("/");
+      console.log(response);
+    } else {
+      alert("Oops! There was an issue commenting on this post!");
     }
+  }
 };
 
 document
-  .querySelector('.comment-form')
-  .addEventListener('submit', newCommentHandler);
+  .querySelector(".comment-form")
+  .addEventListener("submit", newCommentHandler);
